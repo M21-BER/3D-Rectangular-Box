e0f1d37538dd0b246e4905548057ce49fbe55001
M21-BER <94893193+M21-BER@users.noreply.github.com>
2022-12-22 14:59:32 +0300

    script updated in second branch

--- script.js
+++ script.js
@@ -1,70 +1,58 @@
-const rotateX = document.querySelectorAll('.clickX');
-const rotateY = document.querySelectorAll('.clickY');
-const Box = document.querySelector('#cube-box');
-const content = document.querySelector('.content');
+const buttons = document.querySelectorAll(".click");
+const Box = document.querySelector("#cube");
+const content = document.querySelector(".content");
+const year = document.querySelector(".year");
 const para_content = [
-'Beautiful Forest made with Unity',
-'menelik monument found in ethiopia around piassa which is monument built for the famous leader of menelik II',
-'Unity park found in ethiopia around 4 kilo',
-'Lalibela one of the ancient church found in ethiopia which is one of the 7 wonders',
-'inside the deep ocean pleasant view',
-'House model taken from game footage',
- ];
+  '"Beautiful Forest made with Unity"',
+  '"Menelik monument found in ethiopia around piassa which is monument built for the famous leader of menelik II"',
+  '"Unity park found in ethiopia around 4 kilo"',
+  '"Lalibela one of the ancient church found in ethiopia which is one of the 7 wonders"',
+  '"Inside the deep ocean pleasant view"',
+  '"House model taken from game footage"',
+];
 window.onload = content.innerHTML = para_content[0];
-for(let x = 0;x<rotateX.length;x++){
-  rotateX[x].addEventListener('click',()=>{
-  remover(x,'X');
-}) 
-};
+window.onload = year.innerHTML = new Date().getFullYear();
+for (let btn = 0; btn < buttons.length; btn++) {
+  buttons[btn].addEventListener("click", () => {
+    // console.log("clicked btn" + btn);
+    if (btn === 4 || btn === 5 || btn === 2) {
+      transformer(btn, "Y");
+    } else {
+      transformer(btn, "X");
+    }
+  });
+}
 
-for(let y = 0;y<rotateY.length;y++){
-  rotateY[y].addEventListener('click',()=>{
-  remover(y,'Y');
-}) 
-};
-
-
-const remover = (num,dir)=>{
-for(let i = 0;i<Box.classList.length;i++){
+const transformer = (btn, dir) => {
+  for (let i = 0; i < Box.classList.length; i++) {
     Box.classList.remove(Box.classList[i]);
-}
-Box.classList.add('change'+dir+(num+1))
-content_changer('change'+dir+(num+1));
-}   
+  }
+  Box.classList.add("change" + dir + (btn + 1));
+  content_changer("change" + dir + (btn + 1));
+};
 
-const content_changer = (getClass)=>{
- switch(getClass){
-  case 'changeX1':
-     content.innerHTML = para_content[0];
-     break;
-  case 'changeX2':
-     content.innerHTML = para_content[1];
-     break;
-  case 'changeX3':
-     content.innerHTML = para_content[2];
-     break;
-  case 'changeX4':
-     content.innerHTML = para_content[3];
-     break;
-  case 'changeX5':
-     content.innerHTML = para_content[0];
-     break;
-     
-  case 'changeY1':
-     content.innerHTML = para_content[0];
-     break;
-  case 'changeY2':
-     content.innerHTML = para_content[4];
-     break;
-  case 'changeY3':
-     content.innerHTML = para_content[2];
-     break;
-  case 'changeY4':
-     content.innerHTML = para_content[5];
-     break;
-  case 'changeY5':
-     content.innerHTML = para_content[0];
-     break;
-
- }
-}
+const content_changer = (getClass) => {
+  switch (getClass) {
+    case "changeX1":
+      content.innerHTML = para_content[0];
+      break;
+    case "changeX2":
+      content.innerHTML = para_content[1];
+      break;
+    case "changeY3":
+      content.innerHTML = para_content[2];
+      break;
+    case "changeX4":
+      content.innerHTML = para_content[3];
+      break;
+    case "changeY5":
+      content.innerHTML = para_content[4];
+      break;
+    case "changeY6":
+      content.innerHTML = para_content[5];
+      break;
+    default:
+      content.innerHTML = para_content[0];
+      break;
+  }
+};
